bbd11b872355ec6be7c198e70030242dce472ab1
dieptu-4249 <dieptu@kms-technology.com>
2026-07-07 13:47:41 +0700

    first commit

---
+++ GroovyExercise/Scripts/Exercise1/Script1783323721243.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+TestData testData = findTestData("Data Files/Excel Data")
+// get intput data set from Excel file 
+List<Employee> employees = []
+for(int row=1; row<=57; row++) {
+	employees.add(new Employee(testData.getValue(1, row),
+												 testData.getValue(2, row),
+												 testData.getValue(3, row),
+												 testData.getValue(4, row).toInteger(),
+												 testData.getValue(5, row), 
+												 testData.getValue(6, row)));
+}
+// q1: salary of Bradley
+Employee employee1 = employees.find { it.name.contains('Bradley') };
+println "Bradley salary is: ${employee1.salary}";
+
